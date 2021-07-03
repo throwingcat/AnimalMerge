abfac0510adec7d4cde1bc33a2de95016a6a8940
simon <throwingcat@gmail.com>
2021-07-03 22:08:03 +0900

    유닛 생성 딜레이 추가

--- Assets/Script/Define.cs
+++ Assets/Script/Define.cs
@@ -16,4 +16,14 @@ namespace Define
         Korean,
         English,
     }
+
+    public class EnvironmentValue
+    {
+        public static float UNIT_SPAWN_DELAY = 2f;
+        public static float MERGE_DELAY = 0.15f;
+        public static float UNIT_SPRITE_BASE_SIZE = 300f;
+        public static float WORLD_RATIO = 0.00521f;
+
+    }
 }
+
--- Assets/Script/GameCore.cs
+++ Assets/Script/GameCore.cs
@@ -1,29 +1,28 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using Define;
 using UnityEngine;
 using Violet;
 
 public class GameCore : MonoSingleton<GameCore>
 {
+    private float _mergeDelayDelta;
+    private UnitBase _unit;
+    private float _unitSpawnDelayDelta;
     public CSVDownloadConfig CSVDownloadConfig;
+    public Transform UnitParent;
 
     public UnitBase UnitPrefab;
-    public Transform UnitParent;
-    public Vector3 UnitSpawnPosition;
-    public float UnitHorizontalLimit = 333f;
-    private UnitBase _unit;
-
     public List<UnitBase> UnitsInField = new List<UnitBase>();
+    public Vector3 UnitSpawnPosition;
 
     public void OnUpdate(float delta)
     {
         InputUpdate();
 
-        if (_unit == null)
-        {
+        if (_unit == null && _unitSpawnDelayDelta <= 0f)
             _unit = SpawnUnit();
-        }
+        else
+            _unitSpawnDelayDelta -= delta;
 
         MergeUpdate(delta);
     }
@@ -51,20 +50,16 @@ public class GameCore : MonoSingleton<GameCore>
 
     private void RemoveUnit(UnitBase unit)
     {
-        for (int i = 0; i < UnitsInField.Count; i++)
-        {
+        for (var i = 0; i < UnitsInField.Count; i++)
             if (UnitsInField[i].GUID == unit.GUID)
             {
                 UnitsInField.RemoveAt(i);
                 break;
             }
-        }
 
         Destroy(unit.gameObject);
     }
 
-    private float MERGE_DELAY = 0.15f;
-    private float _mergeDelayDelta = 0f;
     private void MergeUpdate(float dleta)
     {
         if (0 < _mergeDelayDelta)
@@ -73,8 +68,8 @@ public class GameCore : MonoSingleton<GameCore>
             return;
         }
 
-        bool isMerge = false;
-        for (int i = 0; i < UnitsInField.Count; i++)
+        var isMerge = false;
+        for (var i = 0; i < UnitsInField.Count; i++)
         {
             var unit = UnitsInField[i];
 
@@ -82,14 +77,14 @@ public class GameCore : MonoSingleton<GameCore>
             {
                 if (unit.GUID == friend.GUID) continue;
                 if (unit.Sheet.key != friend.Sheet.key) continue;
-                float distance = Vector3.Distance(unit.transform.localPosition, friend.transform.localPosition);
-                if (distance <= (unit.transform.localScale.x * 1.26 * 2) + 5)
+                var distance = Vector3.Distance(unit.transform.localPosition, friend.transform.localPosition);
+                if (distance <= unit.transform.localScale.x * 1.26 * 2 + 5)
                 {
                     //Merge 실행
                     isMerge = true;
                     //Remove A B
 
-                    Vector3 pos = new Vector3(
+                    var pos = new Vector3(
                         (unit.transform.localPosition.x + friend.transform.localPosition.x) * 0.5f,
                         (unit.transform.localPosition.y + friend.transform.localPosition.y) * 0.5f,
                         unit.transform.localPosition.z);
@@ -100,7 +95,7 @@ public class GameCore : MonoSingleton<GameCore>
                     if (unit.Sheet.grow_unit.IsNullOrEmpty() == false)
                         UnitsInField.Add(SpawnUnit(unit.Sheet.grow_unit, pos));
 
-                    _mergeDelayDelta = MERGE_DELAY;
+                    _mergeDelayDelta = EnvironmentValue.MERGE_DELAY;
                     break;
                 }
             }
@@ -112,33 +107,27 @@ public class GameCore : MonoSingleton<GameCore>
 
     #region Input
 
-    private bool isPress = false;
+    private bool isPress;
 
     private void InputUpdate()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            OnPress();
-        }
+        if (Input.GetMouseButtonDown(0)) OnPress();
 
-        if (isPress && Input.GetMouseButton(0) == false)
-        {
-            OnRelease();
-        }
+        if (isPress && Input.GetMouseButton(0) == false) OnRelease();
 
         if (isPress)
-        {
             if (_unit != null)
             {
                 var input_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 _unit.transform.position =
                     new Vector3(input_pos.x, _unit.transform.position.y, _unit.transform.position.z);
+
+                var horizontalLimit = Screen.width * 0.5f - (EnvironmentValue.UNIT_SPRITE_BASE_SIZE * EnvironmentValue.WORLD_RATIO * _unit.Sheet.size);
                 _unit.transform.localPosition =
-                    new Vector3(Mathf.Clamp(_unit.transform.localPosition.x, -UnitHorizontalLimit, UnitHorizontalLimit),
+                    new Vector3(Mathf.Clamp(_unit.transform.localPosition.x, -horizontalLimit, horizontalLimit),
                         _unit.transform.localPosition.y,
                         _unit.transform.localPosition.z);
             }
-        }
     }
 
     private void OnPress()
@@ -164,6 +153,7 @@ public class GameCore : MonoSingleton<GameCore>
             _unit.Drop();
             UnitsInField.Add(_unit);
             _unit = null;
+            _unitSpawnDelayDelta = EnvironmentValue.UNIT_SPAWN_DELAY;
         }
     }
 
