a27ded43b0c3a162d1f7d35ecf51fbfcdbed11c6
simon <throwingcat@gmail.com>
2021-07-04 17:55:50 +0900

    인게임 UI 작업중

---
+++ Assets/Script/TextPreset.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Coffee.UIExtensions.UIShadow))]
+public class TextPreset : UnityEngine.UI.Text
+{
+    #if UNITY_EDITOR
+    void Reset()
+    {
+        base.Reset();
+        font = Resources.Load<Font>("Font/Maplestory Bold");
+        Coffee.UIExtensions.UIShadow shadow = GetComponent<Coffee.UIExtensions.UIShadow>();
+        shadow.style = Coffee.UIExtensions.ShadowStyle.Outline8;
+        shadow.effectDistance = new Vector2(2,2);
+        transform.localScale = Vector3.one * 0.5f;
+    }
+    #endif
+}
