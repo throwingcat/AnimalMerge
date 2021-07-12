b6da3bc8306bb2a8c7076cfd21230dcf476a3e5e
simon <throwingcat@gmail.com>
2021-07-13 00:29:40 +0900

    해상도 대응

---
+++ Assets/Script/Uitls/CameraResolution.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResolution : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float scaleHeight = ((float) Screen.width / Screen.height) / ((float) 9 / 16);
+        float scaleWidth = 1f / scaleHeight;
+        if (scaleHeight < 1)
+        {
+            rect.height = scaleHeight;
+            rect.y = (1f - scaleHeight) / 2f;
+        }
+        else
+        {
+            rect.width = scaleWidth;
+            rect.x = (1f - scaleWidth) / 2f;
+        }
+
+        camera.rect = rect;
+    }
+}
