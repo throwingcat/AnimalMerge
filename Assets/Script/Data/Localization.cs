86aaa7b36bf5ca23841a8cfc1b5d3b804195e03e
simon <throwingcat@gmail.com>
2021-07-22 18:09:38 +0900

    텍스트 테이블 반영

---
+++ Assets/Script/Data/Localization.cs
@@ -0,0 +1,12 @@
+using Violet;
+ using System;
+namespace SheetData
+{
+ public partial class Localization : CSVDataBase 
+{ 
+	public string value { get; set; }
+
+	public string Comment { get; set; }
+
+}
+}
